d368d924f0d57c45248b0b4d4df1ac65dc5b2ac3
Isaac Ross <isaacross@Isaacs-MBP.fios-router.home>
2015-08-25 06:16:29 -0400

    applying ignore

--- Filesearcher/.worksheet/src/testbed.scala
+++
@@ -1,4 +0,0 @@
-object testbed {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(60); 
-  println("Welcome to the Scala worksheet");$skip(10); val res$0 = 
-  "Hello";System.out.println("""res0: String("Hello") = """ + $show(res$0))}
-}
--- Filesearcher/target/scala-2.11/classes/testbed.sc
+++
@@ -1,4 +0,0 @@
-object testbed {
-  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
-  "Hello"                                         //> res0: String("Hello") = Hello
-}
